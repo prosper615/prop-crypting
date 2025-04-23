fc1c5fd2b1a996641f3747778594016e2949925e
prosper615 <prosperchiderachukwu@gmail.com>
2025-04-23 02:45:38 -0700

    am hashing the randomly generated range of mersenne numbers which am likely going to use as salt for the algorithm

--- GenerateRandom/HashRandomNumber.js
+++ GenerateRandom/HashRandomNumber.js
@@ -7,16 +7,34 @@
 
 
 
- export function HashRandomNumber( TobeHashed){
 
+
+
+ export  async function HashRandomNumber( TobeHashed){
+
+try {
+    
 
 const EncodeIt = new TextEncoder()
 
-
 const TheEncoded = EncodeIt.encode(TobeHashed)
 
-return TheEncoded
 
+const Hasher = await window.crypto.subtle.digest("SHA-256" , TheEncoded)
+
+
+
+const TurnToUnit = Array.from( new Uint8Array ( Hasher))
+
+const ThenMap = TurnToUnit.map(b =>b.toString(16).padStart(2, "0")).join( "")
+
+return ThenMap
+
+} catch (error) {
+
+    console.log(error)
+    
+}
 
 
 }
