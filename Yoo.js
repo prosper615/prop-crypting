0f7177ed48c1bfbf618e841ff6d890f99b816360
prosper615 <prosperchiderachukwu@gmail.com>
2025-04-23 05:07:39 -0700

    used input element as entry point for the data

--- Yoo.js
+++ Yoo.js
@@ -6,13 +6,74 @@ import { getrandomnumbers } from "./GenerateRandom/RandomNumber.js"
 import { mersenneprime, mersenneprime1, lowerchoosenmerseene,lowerchoosenmerseene1 } from "./GenerateRandom/RandomNumber.js"
 
 
+import { HashRandomNumber } from "./GenerateRandom/HashRandomNumber.js"
+
+
+import { blocksize, calculatepaddingsize,  numberofblocks } from "./Padding/Paddings.js"
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const submitInput = document.getElementById("submitinput");
+
+    const thePlaintextInput = document.getElementById("theplaintextInput");
+
+
+
+    submitInput.addEventListener("click", function() {
+
+      const theInputValue = thePlaintextInput.value;
+
+      let lengthOfText = theInputValue.replace(/ /g, "").length;
+
+      console.log(lengthOfText); 
+
+
+console.log( calculatepaddingsize(lengthOfText))
+
+console.log(numberofblocks(lengthOfText))
+
+
+    });
+
+  });
+
+
+
 
 const fileinput = document.getElementById("fileinput") 
 
 const displayfile = document.getElementById("displayfile") 
 
 
- console.log(getrandomnumbers( mersenneprime1,  lowerchoosenmerseene1 ))
+const seerandom = getrandomnumbers( mersenneprime1,  lowerchoosenmerseene1 )
+
+
+ HashRandomNumber(seerandom).then(hashed=> {console.log(hashed)})
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
