30f6140bccae45002ba9a65d870fd5ba7e1a2404
prosper615 <prosperchiderachukwu@gmail.com>
2025-05-01 18:37:40 -0700

    adding precomputed p arrays

---
+++ KeyExpansion/Parrays.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+/**
+ * P-arrays used in the Blowfish algorithm.
+ * 
+ * These values are derived from the hexadecimal representation of the first 18 numbers after the fractional part of Pi (3.14159...).
+ * 
+ * Specifically, the hexadecimal values are extracted and used as constants in the algorithm.
+ 
+ */
+  
+
+
+
+export const Parrays = [
+
+    // Am using the already precomputed constant instead of trying to do that my self for now
+    
+    0x243F6A88 , 
+     0x85A308D3 , 
+    
+     0x13198A2E, 
+    
+     0x03707344,
+    
+     0xA4093822, 
+    
+     0x299F31D0, 
+    
+     0x082EFA98 ,
+    
+     0xEC4E6C89 , 
+    
+     0x452821E6 , 
+    
+     0x38D01377 ,
+    
+     0xBE5466CF, 
+    
+     0x34E90C6C ,
+    
+     0xC0AC29B7,
+    
+     0xC97C50DD , 
+    
+     0x3F84D5B5 ,
+    
+    0xB5470917 , 
+    
+    0x9216D5D9 , 
+    
+    0x8979FB1B,
+    
+    // The above represent from p1-p18 of the Parrays
+    
+    
+    ]
