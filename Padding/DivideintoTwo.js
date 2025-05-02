f6c1754cebe412c142e77b4fe966a67a518e07ea
prosper615 <prosperchiderachukwu@gmail.com>
2025-05-01 18:33:26 -0700

    // Due to the fact i prefixed Ox to the hex, i would return 10 instead of 8( for now) for it to be 32 bits

--- Padding/DivideintoTwo.js
+++ Padding/DivideintoTwo.js
@@ -1,31 +1,34 @@
 
 
 
-// According to the blowfish algorithm the 64 bit input would be divided into two
+// According to the blowfish algorithm the 64 bit input would be divided into two32 bit halves
 
-// 32 bit halves
 
 
 
 // Now implementing the divsion of the inputs
 
+ export  function divideinputs ( TheacceptedInput){
 
- export function Divideinputs ( TheacceptedInput){
-
-if( TheacceptedInput.length > 16 ){
 
 const theslice = TheacceptedInput.slice( 0, 16)
 
+// Due to the fact i prefixed Ox to the hex, i would return 10 instead of 8 for it to be 32 bits 
 
-const firsthalf = theslice.slice(0,8)
+const firsthalf = theslice.slice(0,10)
 
-const secondhalf = theslice.slice(8)
+const secondhalf = theslice.slice(10)
+
+
+// const firsthalf = theslice.slice(0,8)
+
+// const secondhalf = theslice.slice(8)
+
 
 return {   the32bit : firsthalf , theother32bit: secondhalf}
 
 
 
-}
 
 
 
