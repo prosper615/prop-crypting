7f7869f1e5ac69b34028a2cdd1c3af9904519ede
prosper615 <prosperchiderachukwu@gmail.com>
2025-05-02 17:18:05 -0700

    updated blowfish algo.. , time to check out for some bugs & maybe some testing

--- BlowFishing/blowfishalgo.js
+++ BlowFishing/blowfishalgo.js
@@ -1,68 +1,17 @@
 
 
 
+
+
+
+
 /*
-
-Blowfish uses a 16-round Feistel network and operates on 64-bit blocks.
-
-Each round uses a simple structure involving only:
-
-3 XOR operations
-
-2 additions
-
-4 S-box lookups
-
- Encryption Steps
-Split the 64-bit plaintext into two 32-bit halves:
-
-L0 (left half), R0 (right half)
-
-For rounds 1 to 16 (i = 1 to 16):
-
-Li = Li–1 ⊕ Pi
-
-Ri = f(Li) ⊕ Ri–1
-
-Swap Li and Ri
-
-Undo the final swap:
-
-Swap L16 and R16
-
-Final XOR steps:
-
-R17 = R16 ⊕ P17
-
-L17 = R17 ⊕ P18
-
-Combine L17 and R17 to get the 64-bit ciphertext output.
-
-
-
-*/
-
-
-
-
-
-
-// Now gonna rewrite the blowfish algorithm function i.e delete this stupid one
-
-import { divideinputs } from "../Padding/DivideintoTwo.js";
-
-import { Parrays } from "../KeyExpansion/Parrays.js";
-
-
-import { roundingfunction } from "./RoundFunction.js";
-
-
-
 
    export function blowfishing (templeft , tempright  ){
 
 
 // Am acessing the already divided input in the   ../Padding/DivideintoTwo file path
+
 // const acessleftandright = divideinputs()
 
 // const acessleft = acessleftandright.the32bit
@@ -73,13 +22,13 @@ let lefttemp = templeft
 
 let righttemp = tempright
 
-let holder = []
+// let holder = []
 
 let initail_index = 0
 
-// while (initail_index < Parrays.length) {
 
 
+while (initail_index < Parrays.length) {
 
 lefttemp =  lefttemp ^ Parrays[initail_index]
 
@@ -100,24 +49,126 @@ initail_index++
 
 
 
+if( initail_index  == 16){
 
 
+   lefttemp = righttemp
+
+   righttemp  = lefttemp
 
 
+ const concatleft_and_right = lefttemp + righttemp
 
-
-
+}
 
 
 
     
-// }
+}
 
 
-return Parrays.length
+
+
+
+return concatleft_and_right
+
+
 
 
 
 
 }
+
+*/
+
+
+
+
+
+
+
+import { divideinputs } from "../Padding/DivideintoTwo.js";
+
+import { Parrays } from "../KeyExpansion/Parrays.js";
+
+
+import { roundingfunction } from "./RoundFunction.js";
+
+import { tohex } from "../Padding/Paddings.js";
+
+import { eightbitdivide } from "../Padding/Xl.js";
+
+
+
+
+
+const TheplaintextInput  = document.getElementById("TheplaintextInput")
+
+
+export function blowfishing( ) {
+
+
+     let theInputValue = TheplaintextInput.value
+
+// Get the hex value the user input
+ let thewordandpadding =  tohex(theInputValue )
+
+
+
+ // Extracting the left 32 bit half of the user input
+ 
+ let extracthalf = divideinputs(thewordandpadding)
+
+  let templeft = extracthalf.the32bit
+
+  let tempright = extracthalf.theother32bit
+
+    let initail_index = 0;
+
+
+
+    // would be used to hold the final result  
+    let concatleft_and_right;
+
+    while (initail_index < 16) {
+
+       templeft =  templeft ^ Parrays[initail_index]; 
+
+        let rounded_function = roundingfunction();
+
+        tempright = rounded_function ^ tempright;
+
+        // Swaping values, except on the last round i.e 16th round 
+        if (initail_index < 15) {
+
+            let tempholder = templeft;
+
+           templeft  = tempright;
+
+            tempright = tempholder;
+        }
+
+
+        initail_index++;
+
+    }
+
+    // Final steps after all round
+    tempright = tempright ^ Parrays[16]
+     
+    templeft = templeft ^ Parrays[17]
+
+
+   
+    concatleft_and_right = templeft.toString(16).padStart(8, '0') + tempright.toString(16).padStart(8, '0');
+
+//    concatleft_and_right = templeft + tempright 
+
+
+    return concatleft_and_right
+
+
+}
+
+
 
